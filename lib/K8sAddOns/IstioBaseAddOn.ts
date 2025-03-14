3ec2b3c8752bd495657c28eb93fcf20fe69efe4e
gumonet <gutierrez.monola@gmail.com>
2025-03-13 18:15:01 -0600

    ADD: version to istio

--- lib/K8sAddOns/IstioBaseAddOn.ts
+++ lib/K8sAddOns/IstioBaseAddOn.ts
@@ -1,18 +1,35 @@
-import { Cluster } from "aws-cdk-lib/aws-eks";
+import { Cluster, KubernetesManifest } from "aws-cdk-lib/aws-eks";
 import { createNamespace } from "../utils/cluster-utils";
-export const ISTIO_VERSION = "1.23.2";
+import { ISecurityGroup } from "aws-cdk-lib/aws-ec2";
+export const ISTIO_VERSION = "1.20.3";
 
 export class IstioBaseAddOn {
   constructor(private cluster: Cluster) {}
 
   deploy() {
     const namespace = createNamespace("istio-system", this.cluster);
+    this.addIstioBase(namespace);
+  }
+
+  addIstioBase(namespace: KubernetesManifest | ISecurityGroup) {
     const chartIstioBase = this.cluster.addHelmChart("IstioBase", {
       chart: "base",
       repository: "https://istio-release.storage.googleapis.com/charts",
       namespace: "istio-system",
       release: "istio-base",
       version: ISTIO_VERSION,
+      values: {
+        global: {
+          istiod: {
+            enableAnalysis: false,
+          },
+          configValidation: true,
+          externalIstiod: false,
+          base: {
+            enableIstioConfigCRDs: true,
+          },
+        },
+      },
     });
     chartIstioBase.node.addDependency(namespace);
   }
